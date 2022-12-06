8d7b2fb949b3290a2cecb0a2e955bfcefbd60668
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-05 20:14:14 -0500

    Create ItemsTest.java

---
+++ ItemsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class ItemsTest {
+
+    @Test
+    public void testGetName() {
+        Items item = new Items("TestName");
+        assertTrue(item.getName().equals("TestName"));
+    }
+
+    @Test
+    public void testSetName() {
+        Items item = new Items ("TestName");
+        item.setName("NameTest");
+        assertTrue(item.getName().equals("NameTest"));
+    }
+}
