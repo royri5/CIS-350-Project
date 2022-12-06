760d090597ecb6e12e5e7067fffcc1c0beb31cf8
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-05 20:16:07 -0500

    Create ShieldTest.java

---
+++ ShieldTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class ShieldTest {
+
+    @Test
+    public void testGetName() {
+        Shield shield = new Shield("TestName", 0);
+        assertTrue(shield.getName().equals("TestName"));
+    }
+
+    @Test
+    public void testGetDefense() {
+        Shield shield = new Shield ("TestName", 0);
+        assertTrue(shield.getDefense() == 0);
+    }
+
+    @Test
+    public void testSetName() {
+        Shield shield = new Shield ("TestName", 0);
+        shield.setName("NameTest");
+        assertTrue(shield.getName().equals("NameTest"));
+    }
+
+    @Test
+    public void testSetDefense() {
+        Shield shield = new Shield ("TestName", 0);
+        shield.setDefense(10);
+        assertTrue(shield.getDefense() == 10);
+    }
+
+    @Test
+    public void testSetDefenseNegative() {
+        Shield shield = new Shield ("TestName", 10);
+        shield.setDefense(-10);
+        assertTrue(shield.getDefense() == 0);
+    }
+}
