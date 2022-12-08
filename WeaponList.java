4b06f51375f023754bc6462db938ef7b907fc61e
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-08 13:05:39 -0500

    Update WeaponList.java

--- WeaponList.java
+++ WeaponList.java
@@ -1,4 +1,12 @@
+package backend;
 public class WeaponList {
     
-    public Weapon testWeapon = new Weapon("TestWeapon", "Sword", "d8");
+    public static Weapon testWeapon = new Weapon("TestWeapon", "d8");
+
+    //below is new
+    public static Weapon woodenSword = new Weapon("Wooden Sword", "d4");
+
+    public static Weapon longSword = new Weapon("Long Sword", "d8");
+
+    public static Weapon greatSword = new Weapon("Great Sword", "d12");
  }
