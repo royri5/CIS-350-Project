77eaf16496c271244ea63d4b728cb455ed1e6747
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-08 13:04:41 -0500

    Update ArmorList.java

--- ArmorList.java
+++ ArmorList.java
@@ -1,6 +1,21 @@
+package backend;
 public class ArmorList {
     //Armor testHead = Armor(Armor , head, 0);
-    public Armor testHead = new Armor("TestHead", "Head", 10);
-    public Armor testBody = new Armor("TestBody", "Body", 10);
-    public Armor testLegs = new Armor("TestLegs", "Legs", 10);
+    public static Armor testHead = new Armor("TestHead", "Head", 10);
+    public static Armor testBody = new Armor("TestBody", "Body", 10);
+    public static Armor testLegs = new Armor("TestLegs", "Legs", 10);
+
+    //below is new
+    public static Armor leatherHead = new Armor("Leather Cap", "Head", 2);
+    public static Armor leatherBody = new Armor("Leather Tunic", "Body", 2);
+    public static Armor leatherLegs = new Armor("Leather Trousers", "Legs", 2);
+
+    public static Armor chainHead = new Armor("Chain Cap", "Head", 3);
+    public static Armor chainBody = new Armor("Chain Tunic", "Body", 3);
+    public static Armor chainLegs = new Armor("Chain Trousers", "Legs", 3);
+
+    public static Armor plateHead = new Armor("Plate Helm", "Head", 4);
+    public static Armor plateBody = new Armor("Plate Cuirass", "Body", 4);
+    public static Armor plateLegs = new Armor("Plate Greaves", "Legs", 4);
+
 }
