05492604f512f2c456a7ab049b5e03f549d4fa5e
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-08 13:05:16 -0500

    Update ShieldsList.java

--- ShieldsList.java
+++ ShieldsList.java
@@ -1,4 +1,12 @@
+package backend;
 public class ShieldsList {
     
-    public Shield testShield = new Shield("testShield", 10);
+    public static Shield testShield = new Shield("testShield", 10);
+
+    //below is new
+    public static Shield plankShield = new Shield("Plank Shield", 3);
+
+    public static Shield kiteShield = new Shield("Kite Shield", 4);
+    
+    public static Shield greatShield = new Shield("Great Shield", 5);
 }
