10e10016ff97b4fab92106ced071f0eef13e96d7
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:40:33 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,29 +1,34 @@
-//import java.util.LinkedList;
-//import java.io.*;
+package backend;
+
 import java.util.*;
 
 public class Inventory {
-    
+
     private ArrayList<Items> inventory = new ArrayList<>();
 
+    /**
+     * @return ArrayList<Items>
+     */
     public ArrayList<Items> getInventory() {
         return inventory;
     }
 
+    /**
+     * @param i
+     */
     public void addToInventory(Items i) {
         inventory.add(i);
     }
 
+    /**
+     * @param item
+     */
     public void removeFromInventory(Items item) {
-        for(int i = 0; i < inventory.size(); i++) {
-            if(inventory.get(i).equals(item)) {
+        for (int i = 0; i < inventory.size(); i++) {
+            if (inventory.get(i).equals(item)) {
                 inventory.remove(i);
             }
         }
-    }
-
-    public void printInv() {
-        System.out.println(inventory);
     }
 
 }
