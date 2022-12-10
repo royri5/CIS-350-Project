ef5918cb6128f10ea0343ffa5f29fdb520ef6434
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:46:40 -0500

    Update SkeletalKnight.java

--- SkeletalKnight.java
+++ SkeletalKnight.java
@@ -1,10 +1,14 @@
-public class SkeletalKnight extends Enemy{
-    
+package backend;
+
+public class SkeletalKnight extends Enemy {
+
+    // This is the constructor for the SkeletalKnight class. It is called when a new
+    // SkeletalKnight
+    // object is created.
     public SkeletalKnight() {
         setHp(50);
         setMaxHp(50);
-        setLevel(1);
-        setDefense(15);
+        setDefense(6);
         setDamage("d8");
         setEnemyType("Undead");
     }
