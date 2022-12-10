5643edcd5df31fbb7a18680cc6a141369015e0df
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:35:47 -0500

    Update CombatSystem.java

--- CombatSystem.java
+++ CombatSystem.java
@@ -1,12 +1,12 @@
 package backend;
+
 public class CombatSystem {
 
-
-    
-    
     /**
-     * This function takes two entities and rolls a d20 to see if the first entity can hit the second
-     * entity. If it can, it rolls the damage dice of the first entity and deals that damage to the
+     * This function takes two entities and rolls a d20 to see if the first entity
+     * can hit the second
+     * entity. If it can, it rolls the damage dice of the first entity and deals
+     * that damage to the
      * second entity
      * 
      * @param e1 the attacking entity
@@ -14,15 +14,12 @@ public class CombatSystem {
      */
     public static void weaponAttack(Entity e1, Entity e2) {
         Dice dice = new Dice();
-        //don't remember if d20 can be the same as def
-        //update for crits
-        if(dice.d20() >= e2.getDefense()) {
+        // don't remember if d20 can be the same as def
+        // update for crits
+        if (dice.d20() >= e2.getDefense()) {
             dice.setDiceType(e1.getDamage());
             e2.takeDamage(dice.roll());
         }
     }
-
-
-
 
 }
