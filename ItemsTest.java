6f80fcae665aa4bbd592b42f516925666acc8ddf
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:42:25 -0500

    Update ItemsTest.java

--- ItemsTest.java
+++ ItemsTest.java
@@ -1,5 +1,8 @@
+package backend;
+
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
 public class ItemsTest {
 
     @Test
@@ -10,8 +13,14 @@ public class ItemsTest {
 
     @Test
     public void testSetName() {
-        Items item = new Items ("TestName");
+        Items item = new Items("TestName");
         item.setName("NameTest");
         assertTrue(item.getName().equals("NameTest"));
+    }
+
+    @Test
+    public void testGetItem() {
+        Items item = new Items("testName");
+        assertTrue(item.getItem().equals(item));
     }
 }
