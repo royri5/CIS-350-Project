0e7e29dc85287f2af65e1c5cb2813a78d704bdc6
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:45:58 -0500

    Update ShieldTest.java

--- ShieldTest.java
+++ ShieldTest.java
@@ -1,5 +1,8 @@
+package backend;
+
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
 public class ShieldTest {
 
     @Test
@@ -10,27 +13,27 @@ public class ShieldTest {
 
     @Test
     public void testGetDefense() {
-        Shield shield = new Shield ("TestName", 0);
+        Shield shield = new Shield("TestName", 0);
         assertTrue(shield.getDefense() == 0);
     }
 
     @Test
     public void testSetName() {
-        Shield shield = new Shield ("TestName", 0);
+        Shield shield = new Shield("TestName", 0);
         shield.setName("NameTest");
         assertTrue(shield.getName().equals("NameTest"));
     }
 
     @Test
     public void testSetDefense() {
-        Shield shield = new Shield ("TestName", 0);
+        Shield shield = new Shield("TestName", 0);
         shield.setDefense(10);
         assertTrue(shield.getDefense() == 10);
     }
 
     @Test
     public void testSetDefenseNegative() {
-        Shield shield = new Shield ("TestName", 10);
+        Shield shield = new Shield("TestName", 10);
         shield.setDefense(-10);
         assertTrue(shield.getDefense() == 0);
     }
