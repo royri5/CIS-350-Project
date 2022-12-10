a69ede87449f0fd3ef15be81ecfc297da77ad466
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:35:26 -0500

    Update ArmorTest.java

--- ArmorTest.java
+++ ArmorTest.java
@@ -1,6 +1,8 @@
 package backend;
+
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
 public class ArmorTest {
 
     @Test
@@ -29,59 +31,57 @@ public class ArmorTest {
 
     @Test
     public void testGetDefense() {
-        Armor armor = new Armor ("TestName", "Head", 0);
+        Armor armor = new Armor("TestName", "Head", 0);
         assertTrue(armor.getDefense() == 0);
     }
 
     @Test
     public void testSetSlotHead() {
-        Armor armor = new Armor ("TestName", "Body", 0);
+        Armor armor = new Armor("TestName", "Body", 0);
         armor.setSlot("Head");
         assertTrue(armor.getSlot().equals("Head"));
     }
 
     @Test
     public void testSetSlotBody() {
-        Armor armor = new Armor ("TestName", "Head", 0);
+        Armor armor = new Armor("TestName", "Head", 0);
         armor.setSlot("Body");
         assertTrue(armor.getSlot().equals("Body"));
     }
 
     @Test
     public void testSetSlotLegs() {
-        Armor armor = new Armor ("TestName", "Body", 0);
+        Armor armor = new Armor("TestName", "Body", 0);
         armor.setSlot("Legs");
         assertTrue(armor.getSlot().equals("Legs"));
     }
 
     @Test
     public void testSetSlotnull() {
-        Armor armor = new Armor ("TestName", "Body", 0);
+        Armor armor = new Armor("TestName", "Body", 0);
         armor.setSlot("Gloves");
         assertTrue(armor.getSlot() == null);
     }
 
     @Test
     public void testSetName() {
-        Armor armor = new Armor ("TestName", "Head", 0);
+        Armor armor = new Armor("TestName", "Head", 0);
         armor.setName("NameTest");
         assertTrue(armor.getName().equals("NameTest"));
     }
 
     @Test
     public void testSetDefense() {
-        Armor armor = new Armor ("TestName", "Head", 0);
+        Armor armor = new Armor("TestName", "Head", 0);
         armor.setDefense(10);
         assertTrue(armor.getDefense() == 10);
     }
 
     @Test
     public void testSetDefenseNegative() {
-        Armor armor = new Armor ("TestName", "Head", 10);
+        Armor armor = new Armor("TestName", "Head", 10);
         armor.setDefense(-10);
         assertTrue(armor.getDefense() == 0);
     }
-
-
 
 }
