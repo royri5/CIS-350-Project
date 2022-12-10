b33b2c18c7735bf90e2055ab4400dc78380c4574
royri5 <99297146+royri5@users.noreply.github.com>
2022-12-09 20:42:02 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,25 +1,48 @@
+package backend;
+
 public class Items {
     private String name;
 
+    // A constructor.
     public Items() {
         name = "N/A";
     }
 
+    // A constructor that takes in a string and sets the name of the item to that
+    // string.
     public Items(String name) {
         setName(name);
     }
 
+    /**
+     * This function returns the name of the item.
+     * 
+     * @return The name of the item.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * This function returns the current object.
+     * 
+     * @return The item object itself.
+     */
+    public Items getItem() {
+        return this;
+    }
+
+    /**
+     * If the name is null, set it to N/A, otherwise set it to the name.
+     * 
+     * @param n The name of the item
+     */
     public void setName(String n) {
-        if(n == null) {
+        if (n == null) {
             name = "N/A";
         } else {
             name = n;
         }
     }
-
 
 }
